73489cdebdd9f96a8e449d230d9d5df6f56fc2f0
cowthatprograms <cowthatprograms@gmail.com>
2020-09-08 20:24:38 -0400

    info

--- app.js
+++ app.js
@@ -40,6 +40,47 @@ http.createServer(function(req, res) {
   } else {
     res.writeHead(200, {'Content-Type': 'text/html'});
 
+    var info= {
+
+      timeOpened:new Date(),
+  
+      pageon(){return window.location.pathname},
+      referrer(){return document.referrer},
+      previousSites(){return history.length},
+      browserName(){return navigator.appName},
+      browserEngine(){return navigator.product},
+      browserVersion1a(){return navigator.appVersion},
+      browserVersion1b(){return navigator.userAgent},
+      browserLanguage(){return navigator.language},
+      browserOnline(){return navigator.onLine},
+      browserPlatform(){return navigator.platform},
+      javaEnabled(){return navigator.javaEnabled()},
+      dataCookiesEnabled(){return navigator.cookieEnabled},
+      dataCookies1(){return document.cookie},
+      dataCookies2(){return decodeURIComponent(document.cookie.split(";"))},
+      dataStorage(){return localStorage},
+      sizeScreenW(){return screen.width},
+      sizeScreenH(){return screen.height},
+      sizeDocW(){return document.width},
+      sizeDocH(){return document.height},
+      sizeInW(){return innerWidth},
+      sizeInH(){return innerHeight},
+      sizeAvailW(){return screen.availWidth},
+      sizeAvailH(){return screen.availHeight},
+      scrColorDepth(){return screen.colorDepth},
+      scrPixelDepth(){return screen.pixelDepth},
+      latitude(){return position.coords.latitude},
+      longitude(){return position.coords.longitude},
+      accuracy(){return position.coords.accuracy},
+      altitude(){return position.coords.altitude},
+      altitudeAccuracy(){return position.coords.altitudeAccuracy},
+      heading(){return position.coords.heading},
+      speed(){return position.coords.speed},
+      timestamp(){return position.timestamp}
+    };
+
+    console.log(info);
+
     res.write('<h1>Cat Image Sender</h1>');
     res.write('<form action="sent" method="post" enctype="multipart/form-data">');
     res.write('<label>Send to: </label>');
